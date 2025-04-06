44af7b88fd65245ecbfb60b11efecb1d6d1699e4
jaedeokhan <hjaedeok@gmail.com>
2025-04-06 15:52:50 +0900

    ADD : 스레드 사용 및 문제 풀이 - Runnable을 구현하는 객체를 만들어서 재활용 가능하게 만드는 법도 괜춘

---
+++ java-adv1/src/thread/start/test/sec03/StartTest4Main.java
@@ -0,0 +1,29 @@
+package thread.start.test.sec03;
+
+
+import static util.MyLogger.log;
+
+public class StartTest4Main {
+
+    public static void main(String[] args) {
+        createThread("Thread-A", "A", 1000);
+        createThread("Thread-B", "B", 500);
+    }
+
+    public static void createThread(String threadName, String threadType, int sleepTime) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    log(threadType);
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }, threadName);
+        thread.start();
+    }
+}
