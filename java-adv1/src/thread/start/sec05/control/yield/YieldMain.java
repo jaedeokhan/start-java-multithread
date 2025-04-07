ae2b250830dd13d9308bb6b77a26658986dd7f7f
jaedeokhan <hjaedeok@gmail.com>
2025-04-07 15:53:13 +0900

    ADD : yield - 양보하기 1. Empty : sleep(1)과 yield()를 사용하지 않고 그냥 사용한다면 동일한    스레드가 0~9를 출력하는 경우가 많다. 즉 컨텍스트 스위칭이 자주    일어나지 않는다. 2. sleep(1) : sleep(1)을 사용하면 RUNNABLE -> TIMED_WAITING으로 스레드    상태가 변하기에 컨텍스트 스위칭이 가장 많이 일어난다. 3. yield() : yield()는 다른 스레드에게 OS 우선순위를 양보하게 힌트를    주는 방법이다. 실행 상태는 RUNNABLE으로 유지하낟. 강제는 아니기에 sleep(1) 보다는 컨텍스트 스위칭이 많이    일어나지는 않는다.

---
+++ java-adv1/src/thread/start/sec05/control/yield/YieldMain.java
@@ -0,0 +1,33 @@
+package thread.start.sec05.control.yield;
+
+import static util.ThreadUtils.sleep;
+
+public class YieldMain {
+
+    static final int THREAD_COUNT = 1000;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Thread thread = new Thread(new MyRunnable(), "myRunnable-" + i);
+            thread.start();
+        }
+    }
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                // 1. empty
+                System.out.println(Thread.currentThread().getName() + " - " + i);
+
+                // 2. sleep(1);
+//                sleep(1);
+
+                // 3. Thread.yield();
+                Thread.yield();
+            }
+        }
+    }
+
+}
